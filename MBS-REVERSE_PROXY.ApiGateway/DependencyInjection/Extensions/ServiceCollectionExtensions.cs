8f0c8b86b3b377183b825cf28f3f0237163b029f
bentran1vn <tan182205@gmail.com>
2024-09-16 15:01:58 +0700

    Adding Reverse Proxy Config

---
+++ MBS-REVERSE_PROXY.ApiGateway/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MBS_REVERSE_PROXY.ApiGateway.DependencyInjection.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddReverseProxyApiGateway(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddReverseProxy()
+            .LoadFromConfig(configuration.GetSection("ReverseProxy"));
+    }
+}
--- MBS-REVERSE_PROXY.ApiGateway/Program.cs
+++ MBS-REVERSE_PROXY.ApiGateway/Program.cs
@@ -1,25 +1,16 @@
+using MBS_REVERSE_PROXY.ApiGateway.DependencyInjection.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddJwtAuthenticationApiGateway(builder.Configuration);
+builder.Services.AddReverseProxyApiGateway(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapReverseProxy();
 
 app.Run();
